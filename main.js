0dbb127e984b84ee8926ee961dcff88617126006
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-08-14 19:14:40 +0300

    feat: Add dark mode toggle functionality and styling for theme button

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const menuIcon = document.querySelector(".menu-icon");
+const toggleBtn = document.getElementById("theme-toggle");
 
 menuIcon.addEventListener("click", () => {
   document.querySelector(".navbar").classList.toggle("show");
+});
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  toggleBtn.textContent = document.body.classList.contains("dark-mode")
+    ? "☀️"
+    : "🌙";
 });
